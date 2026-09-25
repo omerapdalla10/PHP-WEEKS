8d0bf4c3e8901740dc3938f670cb8b14c9109b20
Omar Abdullahi <omerapdalla10@gmail.com>
2026-09-25 09:01:47 +0300

    ASSIGMENT

--- ASSIGNMENTS/Assignment4.php
+++ ASSIGNMENTS/Assignment4.php
@@ -7,7 +7,6 @@
 </head>
 <body>
     <?php
-    // write a program that prints number divisibale by 2 and 5 at the same times from 50 to 2
     for ($i = 50; $i >= 2; $i--) {
     if ($i % 2 == 0 && $i % 5 == 0) {
         echo $i . "\n";
